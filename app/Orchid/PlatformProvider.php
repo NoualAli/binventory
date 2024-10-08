090ea0da68e3ecda3ef78a4596155bb6862438b1
Ali NOUAL <noualdev@gmail.com>
2024-10-08 21:31:53 +0100

    Ajout de nouvelle permissions et d'autre sections

--- app/Orchid/PlatformProvider.php
+++ app/Orchid/PlatformProvider.php
@@ -36,19 +36,24 @@ class PlatformProvider extends OrchidServiceProvider
         return [
             Menu::make('Tableau de bord')
                 ->icon('bs.house')
-                ->route(config('platform.index'))
+                ->route(config('platform.index')),
+
+            Menu::make(__('Agences'))
+                ->icon('bs3.bank')
+                ->route('platform.agencies')
+                ->permission('platform.agencies.show')
                 ->divider(),
 
             Menu::make(__('Catégories'))
                 ->icon('bs3.bookmarks')
                 ->route('platform.categories')
-                // ->permission('platform.categories')
+                ->permission('platform.categories.show')
                 ->title(__('Gestion matériel')),
 
-            Menu::make(__('Agences'))
-                ->icon('bs3.bank')
-                ->route('platform.agencies')
-                // ->permission('platform.categories')
+            Menu::make(__('Matériel'))
+                ->icon('bs3.motherboard')
+                ->route('platform.equipments')
+                ->permission('platform.equipments.show')
                 ->divider(),
 
             Menu::make(__('Users'))
@@ -75,8 +80,22 @@ class PlatformProvider extends OrchidServiceProvider
         return [
             ItemPermission::group(__('System'))
                 ->addPermission('platform.systems.roles', __('Roles'))
-                ->addPermission('platform.systems.users', __('Users'))
-                ->addPermission('platform.categories', __('Catégories')),
+                ->addPermission('platform.systems.users', __('Users')),
+                ItemPermission::group(__('Catégories'))
+                ->addPermission('platform.categories.show', __('Voir'))
+                ->addPermission('platform.categories.create', __('Créer'))
+                ->addPermission('platform.categories.edite', __('Modifier'))
+                ->addPermission('platform.categories.delete', __('Supprimer')),
+                ItemPermission::group(__('Agences'))
+                ->addPermission('platform.agencies.show', __('Voir'))
+                ->addPermission('platform.agencies.create', __('Créer'))
+                ->addPermission('platform.agencies.edite', __('Modifier'))
+                ->addPermission('platform.agencies.delete', __('Supprimer')),
+                ItemPermission::group(__('Matériels'))
+                ->addPermission('platform.equipments.show', __('Voir'))
+                ->addPermission('platform.equipments.create', __('Créer'))
+                ->addPermission('platform.equipments.edit', __('Modifier'))
+                ->addPermission('platform.equipments.delete', __('Supprimer'))
         ];
     }
 }
