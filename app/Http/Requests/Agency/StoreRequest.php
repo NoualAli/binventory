c696d5ac3e7b66006106b463ea34ac8af318d24f
Ali NOUAL <noualdev@gmail.com>
2024-10-08 21:28:50 +0100

    Ajout de la validation des agences

--- app/Http/Requests/Agency/StoreRequest.php
+++ app/Http/Requests/Agency/StoreRequest.php
@@ -11,7 +11,7 @@ class StoreRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return can('platform.agencies.create');
     }
 
     /**
